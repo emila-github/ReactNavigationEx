ec0f06ef3a2a778291b4d360fa7c99f889d7f4a7
changjianwang <changjianwang@cyou-inc.com>
2017-07-20 18:19:16 +0800

    调整入口文件

--- AppExpo/screens/HomeScreen.js
+++ AppExpo/screens/HomeScreen.js
@@ -11,39 +11,8 @@ import {
 import { TabNavigator, StackNavigator } from 'react-navigation';``
 import Ionicons from 'react-native-vector-icons/Ionicons';
 
-import HelloWorld from '../components/HomeScreen/BasiceTab/HelloWorld';
-import Props from '../components/HomeScreen/BasiceTab/Props';
-import State from '../components/HomeScreen/BasiceTab/State';
-import Style from '../components/HomeScreen/BasiceTab/Style';
-import HeightAndWidth from '../components/HomeScreen/BasiceTab/HeightAndWidth';
+import BasicsExampleRoutes from '../components/HomeScreen/BasiceTab/index';
 
-const BasicsExampleRoutes = {
-  HelloWorld: {
-    name: 'Hello World',
-    description: 'Hello World!',
-    screen: HelloWorld,
-  },
-  Props: {
-    name: 'Props',
-    description: '大多数组件在创建时就可以使用各种参数来进行定制。用于定制的这些参数就称为props（属性）。',
-    screen: Props,
-  },
-  State: {
-    name: 'State',
-    description: '我们使用两种数据来控制一个组件：props和state。props是在父组件中指定，而且一经指定，在被指定的组件的生命周期中则不再改变。 对于需要改变的数据，我们需要使用state。',
-    screen: State,
-  },
-  Style: {
-    name: 'Style',
-    description: '所有的核心组件都接受名为style的属性。这些样式名基本上是遵循了web上的CSS的命名，只是按照JS的语法要求使用了驼峰命名法，例如将background-color改为backgroundColor',
-    screen: Style,
-  },
-  HeightAndWidth: {
-    name: 'Height and Width',
-    description: '高度与宽度,React Native中的尺寸都是无单位的，表示的是与设备像素密度无关的逻辑像素点',
-    screen: HeightAndWidth,
-  },
-};
 
 // Object.keys(BasicsExampleRoutes).map(routeName => {
 //   BasicsExampleRoutes[routeName].navigationOptions = {
