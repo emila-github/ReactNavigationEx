6e87883842c8d9bf24cd58ec5f4a29f39e913587
changjianwang <changjianwang@cyou-inc.com>
2017-07-20 18:11:14 +0800

    固定宽高

--- AppExpo/screens/HomeScreen.js
+++ AppExpo/screens/HomeScreen.js
@@ -14,6 +14,8 @@ import Ionicons from 'react-native-vector-icons/Ionicons';
 import HelloWorld from '../components/HomeScreen/BasiceTab/HelloWorld';
 import Props from '../components/HomeScreen/BasiceTab/Props';
 import State from '../components/HomeScreen/BasiceTab/State';
+import Style from '../components/HomeScreen/BasiceTab/Style';
+import HeightAndWidth from '../components/HomeScreen/BasiceTab/HeightAndWidth';
 
 const BasicsExampleRoutes = {
   HelloWorld: {
@@ -30,6 +32,16 @@ const BasicsExampleRoutes = {
     name: 'State',
     description: '我们使用两种数据来控制一个组件：props和state。props是在父组件中指定，而且一经指定，在被指定的组件的生命周期中则不再改变。 对于需要改变的数据，我们需要使用state。',
     screen: State,
+  },
+  Style: {
+    name: 'Style',
+    description: '所有的核心组件都接受名为style的属性。这些样式名基本上是遵循了web上的CSS的命名，只是按照JS的语法要求使用了驼峰命名法，例如将background-color改为backgroundColor',
+    screen: Style,
+  },
+  HeightAndWidth: {
+    name: 'Height and Width',
+    description: '高度与宽度,React Native中的尺寸都是无单位的，表示的是与设备像素密度无关的逻辑像素点',
+    screen: HeightAndWidth,
   },
 };
 
